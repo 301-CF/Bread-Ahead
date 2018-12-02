fae43624ff35641498ff919c0c488c182301605a
J <jchristie1@gmail.com>
2018-12-02 11:37:17 -0700

    Bread calc w/ differnt tools/frameworks/ideas. Work in progress

--- static/test.js
+++ static/test.js
@@ -1,62 +1,61 @@
 function gramToPercent(grams, flour) {
-  return +((grams / flour) * 100).toFixed(2);
+    return +((grams / flour) * 100).toFixed(2);
 }
 
 function percentToGram(percent, flour) {
-  return +((percent * flour) / 100).toFixed(2);
+    return +((percent * flour) / 100).toFixed(2);
 }
 
 const masterBread = 1000;
 let hydro = 0;
-let salt  = 0;
-let rise  = 0;
+let salt = 0;
+let rise = 0;
 
 
 function updateTotal(hydro, salt, rise) {
-  hydro = Number(document.getElementById('hydro-g').value);
-  salt = Number(document.getElementById('salt-g').value);
-  rise = Number(document.getElementById('rise-g').value);
-  let gramTotal = hydro + salt + rise + masterBread;
+    hydro = Number(document.getElementById('hydro-g').value);
+    salt = Number(document.getElementById('salt-g').value);
+    rise = Number(document.getElementById('rise-g').value);
+    let gramTotal = hydro + salt + rise + masterBread;
 
-  if (gramTotal<=10000) {
-    return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal}g`);
+    if (gramTotal <= 10000) {
+        return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal}g`);
 
-  } else if (gramTotal<=1000000) {
-    return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal/1000}kg`);
+    } else if (gramTotal <= 1000000) {
+        return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal / 1000}kg`);
 
-  } else if (gramTotal>1000000 && gramTotal<100000000){
-    return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal/1000000}Mg`);
+    } else if (gramTotal > 1000000 && gramTotal < 100000000) {
+        return document.getElementById('master-total').innerHTML = (`TOTAL ${gramTotal / 1000000}Mg`);
 
-  } else {
-    return document.getElementById('master-total').innerHTML = (`WOAH THERE!`);
-  }
+    } else {
+        return document.getElementById('master-total').innerHTML = (`WOAH THERE!`);
+    }
 }
 
 // Hydration
-$('#hydro-g').on('input', function () {
-  $("#hydro-p").val(gramToPercent(Number($(this).val()), masterBread))
+$('#hydro-g').on('input', function() {
+    $("#hydro-p").val(gramToPercent(Number($(this).val()), masterBread))
 });
 
-$('#hydro-p').on('input', function () {
- $("#hydro-g").val(percentToGram(Number($(this).val()), masterBread))
+$('#hydro-p').on('input', function() {
+    $("#hydro-g").val(percentToGram(Number($(this).val()), masterBread))
 });
 
 
 // Salt
-$('#salt-g').on('input', function () {
-  $("#salt-p").val(gramToPercent(Number($(this).val()), masterBread))
+$('#salt-g').on('input', function() {
+    $("#salt-p").val(gramToPercent(Number($(this).val()), masterBread))
 });
 
-$('#salt-p').on('input', function () {
-  $("#salt-g").val(percentToGram(Number($(this).val()), masterBread))
+$('#salt-p').on('input', function() {
+    $("#salt-g").val(percentToGram(Number($(this).val()), masterBread))
 });
 
 // Rise/Yeast
-$('#rise-g').on('input', function () {
-  $("#rise-p").val(gramToPercent(Number($(this).val()), masterBread))
+$('#rise-g').on('input', function() {
+    $("#rise-p").val(gramToPercent(Number($(this).val()), masterBread))
 });
 
-$('#rise-p').on('input', function () {
-  $("#rise-g").val(percentToGram(Number($(this).val()), masterBread));
+$('#rise-p').on('input', function() {
+    $("#rise-g").val(percentToGram(Number($(this).val()), masterBread));
 });
-
---
+++ style-bread/README.md
@@ -0,0 +1,13 @@
+# Set up a Node + Express + Nodemon
+
+## Project Set up
+
+1. Make project folder
+1. npm init
+1. npm install express --save
+1. npm install -g nodemon (should be installed globally if using the -g flag before)
+
+## Run commands
+
+* node app.js
+* nodemon app.js
