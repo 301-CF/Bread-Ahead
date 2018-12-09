ce4cf2eb1cdaa3fc8daf4d94239835fa79fbb863
J <jchristie1@gmail.com>
2018-12-08 23:26:34 -0700

    Bread refactor into Vue

--- vue-bread/src/App.vue
+++ vue-bread/src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/calc">Calc</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+  html, body {
+    height: 100%;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,6 +21,9 @@
   color: #2c3e50;
 }
 #nav {
+  width: 100%;
+  margin: 0;
+  background-image: linear-gradient(to right, #a25b5b, #692e4c);
   padding: 30px;
   a {
     font-weight: bold;
---
+++ vue-bread/src/views/Calc.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home">
+    <br>
+    <h1>CALCULATOR!</h1>
+    <br>
+    <section>
+      <div class="col-master">
+        <h1> MASTER FORMULA</h1>
+        <div class="row">
+          <strong>Flour</strong>
+          <label><input id="master-flour-gram">g</label>
+          <label><input id="master-flour-percent" readonly placeholder="100">%</label>
+        </div>
+        <div class="row">
+          <strong>Hydro</strong>
+          <label><input id="master-hydro-gram">g</label>
+          <label><input id="master-hydro-percent">%</label>
+        </div>
+        <div class="row">
+          <strong>Salt</strong>
+          <label><input id="master-salt-gram">g</label>
+          <label><input id="master-salt-percent">%</label>
+        </div>
+      </div>
+    </section>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  html, body, h1, nav {
+    margin: 0;
+    padding: 0;
+  }
+  html, body {
+    height: 100%;
+    background-color: dimgrey;
+  }
+  nav{
+    width: 100%;
+  }
+  .row {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+  input {
+    direction: rtl;
+    border-radius: 6px;
+  }
+</style>
+
+
+<!--<script>-->
+// @ is an alias to /src
+import input from '@/components/input.vue';
+// import calc from '@/components/calc.vue';
+// import HelloWorld from '@/components.vue.';
+
+<!--export default {-->
+  <!--name: 'home',-->
+  <!--components: {-->
+    <!--HelloWorld,-->
+  <!--},-->
+<!--};-->
+<!--</script>-->
